150c0a4697ed6aa3764c02b635ce7a5c815f6907
erik <erik.slovik1111@gmail.com>
2025-01-09 23:51:35 +0100

    update

--- scripts/enemy_selection.gd
+++ scripts/enemy_selection.gd
@@ -9,14 +9,34 @@ func _ready():
 
 func select_enemy():
 	enemy = EnemyManager.enemy_type
-	if enemy == "Skeleton":
-		var skeleton = preload("res://scenes/enemies/skeleton_enemy_type.tscn").instantiate()
-		skeleton.global_position = $Controllable_player.global_position
-		add_child(skeleton)
-	elif enemy == "Spider":
+	if enemy == "Spider":
 		var spider = preload("res://scenes/enemies/spider_enemy_type.tscn").instantiate()
 		spider.global_position = $Controllable_player.global_position
 		add_child(spider)
+	elif enemy == "Rat":
+		var rat = preload("res://scenes/enemies/rat_enemy_type.tscn").instantiate()
+		rat.global_position = $Controllable_player.global_position
+		add_child(rat)
+	elif enemy == "Hag":
+		var hag = preload("res://scenes/enemies/hag_enemy_type.tscn").instantiate()
+		hag.global_position = $Controllable_player.global_position
+		add_child(hag)
+	elif enemy == "Ghost":
+		var ghost = preload("res://scenes/enemies/ghost_enemy_type.tscn").instantiate()
+		ghost.global_position = $Controllable_player.global_position
+		add_child(ghost)
+	elif enemy == "Ghoul":
+		var ghoul = preload("res://scenes/enemies/ghoul_enemy_type.tscn").instantiate()
+		ghoul.global_position = $Controllable_player.global_position
+		add_child(ghoul)
+	elif enemy == "Zombie":
+		var zombie = preload("res://scenes/enemies/zombie_enemy_type.tscn").instantiate()
+		zombie.global_position = $Controllable_player.global_position
+		add_child(zombie)
+	elif enemy == "Skeleton":
+		var skeleton = preload("res://scenes/enemies/skeleton_enemy_type.tscn").instantiate()
+		skeleton.global_position = $Controllable_player.global_position
+		add_child(skeleton)
 	elif enemy == "Skeleton Mage":
 		var skeleton_mage = preload("res://scenes/enemies/skeleton_mage_enemy_type.tscn").instantiate()
 		skeleton_mage.global_position = $Controllable_player.global_position
